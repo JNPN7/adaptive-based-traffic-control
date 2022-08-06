0b768608e8da52179acd8734582baebf71a4638c
JNPN7 <jpphanju54@gmail.com>
2022-08-06 13:49:19 +0545

    movement

--- src/traffic-simulator/simulate.py
+++ src/traffic-simulator/simulate.py
@@ -1,11 +1,13 @@
-import imp
+from filecmp import dircmp
 import sys, pygame
+from xml.etree.ElementPath import find
 from road import draw_road
 from traffic_light import TrafficLight
-
+import random
 import threading
 from vehicles import *
 from variables import *
+from time import sleep
 
 pygame.init()
 
@@ -32,22 +34,75 @@ traffic_lights = [
                  WIDTH / 2 + ROAD_WIDTH / 2 * 1.4, 'B'),
 ]
 
-v1 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.left, 
-             Direction.left)
-v2 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.left,
-             Direction.right)
-v3 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.right,
-             Direction.right)
-v4 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.right,
-             Direction.up)
-v5 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.up,
-             Direction.up)
-v6 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.up,
-             Direction.down)
-v7 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.down,
-             Direction.down)
-v8 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.down,
-             Direction.right)
+# v1 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.left, 
+#              Direction.left)
+# v2 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.left,
+#              Direction.right)
+# v3 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.right,
+#              Direction.right)
+# v4 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.right,
+#              Direction.up)
+# v5 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.up,
+#              Direction.up)
+# v6 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.up,
+#              Direction.down)
+# v7 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.left, Direction.down,
+#              Direction.down)
+# v8 = Vehicle(0.1, ROAD_WIDTH / 6, ROAD_WIDTH / 6, Lane.right, Direction.down,
+#              Direction.right)
+vehicles = []
+vehicles_dict = {
+    Direction.left: {
+        # Direction.right: [],
+        # Direction.up: [],
+        # Direction.down: []
+        Lane.left: [],
+        Lane.right: []
+    },
+    Direction.right: {
+        # Direction.left: [],
+        # Direction.up: [],
+        # Direction.down: []
+        Lane.left: [],
+        Lane.right: []
+    },
+    Direction.up: {
+        # Direction.left: [],
+        # Direction.right: [],
+        # Direction.down: []
+        Lane.left: [],
+        Lane.right: []
+    },
+    Direction.down: {
+        # Direction.left: [],
+        # Direction.right: [],
+        # Direction.up: []
+        Lane.left: [],
+        Lane.right: []
+    }
+}
+
+def find_associate_light(vehicle):
+    if vehicle.direction == Direction.left:
+        return traffic_lights[0]
+    elif vehicle.direction == Direction.right:
+        return traffic_lights[1]
+    elif vehicle.direction == Direction.up:
+        return traffic_lights[2]
+    return traffic_lights[3]
+
+def generate_vehicle():
+    while True: 
+        lane = Lane(random.randint(1, 2))
+        direction = Direction(random.randint(1, 4))
+        lst = list(range(1, 5))
+        del lst[direction.value - 1]
+        dest_direciton = Direction(random.choice(lst))
+        vehicle = Vehicle(0.2, ROAD_WIDTH / 6, ROAD_WIDTH / 6, lane, direction, dest_direciton)
+        vehicles.append(vehicle)
+        vehicles_dict[direction][lane].append(vehicle)
+        # print(vehicles_dict)
+        sleep(1)
 
 def initialize_lights():
     while True:
@@ -57,6 +112,10 @@ def initialize_lights():
 traffic_light_thread = threading.Thread(name="initialization", target=initialize_lights, args=())
 traffic_light_thread.daemon = True
 traffic_light_thread.start()
+
+generate_vehicle_thread = threading.Thread(name="Initialization", target=generate_vehicle, args=())
+generate_vehicle_thread.daemon = True
+generate_vehicle_thread.start()
 
 
 while 1:
@@ -79,23 +138,28 @@ while 1:
     # print(f'bottom light status: {traffic_lights[3].get_status()}')
 
     draw_road(screen, WIDTH, HEIGHT, ROAD_WIDTH)
-    v1.draw(screen)
-    v2.draw(screen)
-    v3.draw(screen)
-    v4.draw(screen)
-    v5.draw(screen)
-    v6.draw(screen)
-    v7.draw(screen)
-    v8.draw(screen)
+    # v1.draw(screen)
+    # v2.draw(screen)
+    # v3.draw(screen)
+    # v4.draw(screen)
+    # v5.draw(screen)
+    # v6.draw(screen)
+    # v7.draw(screen)
+    # v8.draw(screen)
 
-    v1.move()
-    v2.move()
-    v3.move()
-    v4.move()
-    v5.move()
-    v6.move()
-    v7.move()
-    v8.move()
+    for vehicle in vehicles:
+        vehicle.draw(screen)
+        light = find_associate_light(vehicle)
+        vehicle.move(vehicles_dict, light)
+
+    # v1.move()
+    # v2.move()
+    # v3.move()
+    # v4.move()
+    # v5.move()
+    # v6.move()
+    # v7.move()
+    # v8.move()
 
     pygame.display.flip()
 
