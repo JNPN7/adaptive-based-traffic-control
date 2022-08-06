a68870760224d82ec32c2b6513014820207e5f25
juju2181 <anishshilpakar8@gmail.com>
2022-08-06 08:34:23 +0545

    Corrected Zebra crossing line

--- src/traffic-simulator/road.py
+++ src/traffic-simulator/road.py
@@ -1,5 +1,5 @@
 import pygame
-from draw_dashed import dashed_rect
+from draw_dashed import *
 
 def draw_road(screen, width, height, road_width):
     wc, hc = width/2, height/2
@@ -18,16 +18,23 @@ def draw_road(screen, width, height, road_width):
     pygame.draw.rect(screen, 'black', (hc-road_width/2, 0, road_width, width))
 
     # median strip
-    dashed_rect(screen, 'yellow', (0,hc,wc-zebra_crossing_dist,hc),width=3)
-    dashed_rect(screen, 'yellow', (wc+zebra_crossing_dist,hc,width,hc),width=3)
-    dashed_rect(screen, 'yellow', (wc,0,wc,hc-zebra_crossing_dist),width=3)
-    dashed_rect(screen, 'yellow', (wc,hc+zebra_crossing_dist,wc,height),width=3)
+    dashed_rect(screen, 'yellow', (0,hc,wc-zebra_crossing_dist,hc),width=3,dash_length=20)
+    dashed_rect(screen, 'yellow', (wc+zebra_crossing_dist,hc,width,hc),width=3,dash_length=20)
+    dashed_rect(screen, 'yellow', (wc,0,wc,hc-zebra_crossing_dist),width=3,dash_length=20)
+    dashed_rect(screen, 'yellow', (wc,hc+zebra_crossing_dist,wc,height),width=3,dash_length=20)
 
 
 
     # pygame.draw.rect(screen, 'yellow', pygame.Rect(wc - road_width/2 - 20 , hc - road_width/2 - 20, road_width + 40, road_width + 40), 5)
 
-    pygame.draw.line(screen, 'white', zebra_crossing["left"][0], zebra_crossing["left"][1], 5)
-    pygame.draw.line(screen, 'white', zebra_crossing["right"][0], zebra_crossing["right"][1], 5)
-    pygame.draw.line(screen, 'white', zebra_crossing["top"][0], zebra_crossing["top"][1], 5)
-    pygame.draw.line(screen, 'white', zebra_crossing["bottom"][0], zebra_crossing["bottom"][1], 5)
+    # zebra crossing line
+    # pygame.draw.line(screen, 'white', zebra_crossing["left"][0], zebra_crossing["left"][1], 5)
+    # pygame.draw.line(screen, 'white', zebra_crossing["right"][0], zebra_crossing["right"][1], 5)
+    # pygame.draw.line(screen, 'white', zebra_crossing["top"][0], zebra_crossing["top"][1], 5)
+    # pygame.draw.line(screen, 'white', zebra_crossing["bottom"][0], zebra_crossing["bottom"][1], 5)
+
+    # zebra crossing
+    dashed_zebra_line(screen, 'white', zebra_crossing["left"][0],zebra_crossing["left"][1],width=10,dash_length=1)
+    dashed_zebra_line(screen, 'white', zebra_crossing["right"][0],zebra_crossing["right"][1],width=10,dash_length=1)
+    dashed_zebra_line(screen, 'white', zebra_crossing["top"][0],zebra_crossing["top"][1],width=10,dash_length=1)
+    dashed_zebra_line(screen, 'white', zebra_crossing["bottom"][0],zebra_crossing["bottom"][1],width=10,dash_length=1)
